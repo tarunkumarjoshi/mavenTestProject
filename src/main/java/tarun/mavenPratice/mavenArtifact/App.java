20fd1ef43619a8f6ddfe5aae4ce00c5c183abacd
tarunkumarjoshi <tarunkumarjoshi@gmail.com>
2019-03-03 10:32:01 -0600

    added repo

---
+++ src/main/java/tarun/mavenPratice/mavenArtifact/App.java
@@ -0,0 +1,13 @@
+package tarun.mavenPratice.mavenArtifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
